a6e64b6a9938cd4f328ae7d0735cbf38095d1a5d
Brenda J <linjiangda@yahoo.com>
2021-06-14 11:43:25 -0400

    mern

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require("passport");
+const path = require('path');
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
@@ -21,3 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
